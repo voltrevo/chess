9f823f2f40b1c3d46f61a4dd056f276acc2c96e3
Andrew Morris <voltrevo@gmail.com>
2017-04-16 14:40:10 +1000

    Implement async move picking

--- src/pickMoveByRating.ts
+++ src/pickMoveByRating.ts
@@ -47,3 +47,56 @@ export const applyDepth:
 export const pickMoveByRating = (board: Uint8Array, rateBoard: (board: Uint8Array) => number) => {
   return findBestMoveAndRating(board, rateBoard)[0];
 };
+
+export const applyPromise:
+  (rateBoard: (board: Uint8Array) => number) =>
+  (board: Uint8Array) => Promise<number>
+= (rateBoard: (board: Uint8Array) => number) => {
+  return (board: Uint8Array) => new Promise(resolve =>
+    setTimeout(() => resolve(rateBoard(board)))
+  );
+};
+
+const findBestMoveAndRatingPromise = (board: Uint8Array, rate: (board: Uint8Array) => Promise<number>) => {
+  let bestMove: [number, number] | null = null;
+  let bestRating = -Infinity;
+
+  const ratingMultiplier = (board[64] === codes.sides.white) ? 1 : -1;
+
+  const moves = Array.from(findAllMoves(board));
+
+  type MoveAndRating = [[number, number] | null, number];
+
+  const moveAndRatingsPromise = Promise
+    .all(
+      moves.map(move => rate(applyMove(board, move))
+        .then(rating => [move, ratingMultiplier * rating] as MoveAndRating)
+      )
+    )
+  ;
+
+  return moveAndRatingsPromise
+    .then((moveAndRatings: MoveAndRating[]) => {
+      // TODO: If no moves found, check for stalemate
+      return moveAndRatings.reduce((a: MoveAndRating, b: MoveAndRating) => a[1] > b[1] ? a : b)
+    })
+  ;
+};
+
+export const applyDepthPromise:
+  (rateBoard: (board: Uint8Array) => Promise<number>, depth: number) =>
+  (board: Uint8Array) => Promise<number>
+= (rateBoard: (board: Uint8Array) => Promise<number>, depth: number) => {
+  if (depth === 0) {
+    return rateBoard;
+  }
+
+  const rate = applyDepthPromise(rateBoard, depth - 1);
+
+  return (board: Uint8Array) => findBestMoveAndRatingPromise(board, rate)
+    .then(moveAndRating => moveAndRating[1]);
+};
+
+export const pickMoveByRatingPromise = (board: Uint8Array, rateBoard: (board: Uint8Array) => Promise<number>) => {
+  return findBestMoveAndRatingPromise(board, rateBoard).then(moveAndRating => moveAndRating[0]);
+};

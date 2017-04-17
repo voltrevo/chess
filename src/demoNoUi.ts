07fe745a57f6ef7c4781f86cdb82d4473ac1e6b8
Andrew Morris <voltrevo@gmail.com>
2017-04-17 17:16:13 +1000

    Split out runChessGame logic

--- src/demoNoUi.ts
+++ src/demoNoUi.ts
@@ -21,6 +21,7 @@ import {
 
 import { rateBoard } from './rateBoard';
 import { rateBoardChallenger } from './rateBoardChallenger';
+import { runChessGame } from './runChessGame';
 import { entries, values } from './util';
 
 const applyDeep = (rate: (board: Uint8Array) => number) => {
@@ -34,63 +35,15 @@ const applyDeep = (rate: (board: Uint8Array) => number) => {
 const rateBoardDeep = applyDeep(rateBoard);
 const rateBoardChallengerDeep = applyDeep(rateBoardChallenger);
 
-(() => {
-  const trial: () => Promise<boolean> = () => {
-    let board = newGame;
-    let moves = 0;
+const startTime = Date.now();
 
-    // TODO: Better side alternation (should be A-B-B-A-A-B-B-...)
-    const challengerSide = (Math.random() < 0.5 ? codes.sides.white : codes.sides.black);
-    const getRater = () => {
-      return (board[64] === challengerSide ? rateBoardChallengerDeep : rateBoardDeep);
-    };
-
-    const computerMove = () => {
-      const rater = getRater();
-
-      return pickMoveByRatingPromise(board, getRater(), Math.random()).then((move) => {
-        if (move !== null) {
-          board = applyMove(board, move);
-          return { gameOver: false };
-        }
-
-        return { gameOver: true };
-      });
-    };
-
-    const loop: () => Promise<boolean> = () => computerMove().then(({ gameOver }) => {
-      moves++;
-
-      if (moves > 500) {
-        console.log('Restarting due to >500 moves');
-        return trial();
-      }
-
-      if (!gameOver) {
-        return loop();
-      }
-
-      if (determineEndState(board) === 'stalemate') {
-        return trial();
-      }
-
-      // If it's checkmate and non-challenger turn, then challenger won
-      return board[64] !== challengerSide;
-    });
-
-    return loop();
-  };
-
-  const startTime = Date.now();
-
-  headToHead(
-    trial,
-    update => console.log(JSON.stringify({
-      wins: update.wins,
-      losses: update.losses,
-      score: `${(100 * update.score).toFixed(1)}%`,
-      confidence: `${(100 * update.confidence).toFixed(1)}%`,
-      time: `${Math.floor((Date.now() - startTime) / 60000)}min`,
-    })),
-  ).catch(err => console.error(err));
-})();
+headToHead(
+  () => runChessGame(rateBoardDeep, rateBoardChallengerDeep),
+  update => console.log(JSON.stringify({
+    wins: update.wins,
+    losses: update.losses,
+    score: `${(100 * update.score).toFixed(1)}%`,
+    confidence: `${(100 * update.confidence).toFixed(1)}%`,
+    time: `${Math.floor((Date.now() - startTime) / 60000)}min`,
+  })),
+).catch(err => console.error(err));
---
+++ src/runChessGame.ts
@@ -0,0 +1,50 @@
+import { applyMove, codes, newGame } from './board';
+import { determineEndState, pickMoveByRatingPromise } from './pickMoveByRating';
+
+type RaterT = (board: Uint8Array) => Promise<number>;
+
+export const runChessGame: (rate: RaterT, rateChallenger: RaterT) => Promise<boolean> = (rate, rateChallenger) => {
+  let board = newGame;
+  let moves = 0;
+
+  // TODO: Better side alternation (should be A-B-B-A-A-B-B-...)
+  const challengerSide = (Math.random() < 0.5 ? codes.sides.white : codes.sides.black);
+  const getRater = () => {
+    return (board[64] === challengerSide ? rateChallenger : rate);
+  };
+
+  const computerMove = () => {
+    const rater = getRater();
+
+    return pickMoveByRatingPromise(board, getRater(), Math.random()).then((move) => {
+      if (move !== null) {
+        board = applyMove(board, move);
+        return { gameOver: false };
+      }
+
+      return { gameOver: true };
+    });
+  };
+
+  const loop: () => Promise<boolean> = () => computerMove().then(({ gameOver }) => {
+    moves++;
+
+    if (moves > 500) {
+      console.log('Restarting due to >500 moves');
+      return runChessGame(rate, rateChallenger);
+    }
+
+    if (!gameOver) {
+      return loop();
+    }
+
+    if (determineEndState(board) === 'stalemate') {
+      return runChessGame(rate, rateChallenger);
+    }
+
+    // If it's checkmate and non-challenger turn, then challenger won
+    return board[64] !== challengerSide;
+  });
+
+  return loop();
+};

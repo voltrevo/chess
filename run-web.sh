b6464c40f6f20f282c09e1c2f43397681e501415
Andrew Morris <voltrevo@gmail.com>
2018-11-30 21:32:42 +1100

    Minor fixes

--- run-web.sh
+++ run-web.sh
@@ -7,6 +7,6 @@ mkdir ./build/web
 ln -s $(pwd)/node_modules/chessboardjs/www/img ./build/web/img
 ln -s $(pwd)/node_modules/chessboardjs/www/css ./build/web/css
 
-cp ./index.html ./build/web/.
+cp ./web/static/index.html ./build/web/.
 
 ./node_modules/.bin/static ./build/web
